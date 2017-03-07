3c6a2e5abd586af02c2ea652220fa91434e6f7ff
Madhubabu <mpoda@capspayroll.com>
2017-03-07 20:18:27 +0530

    User screen validations completed

--- app/models/generic_info.rb
+++ app/models/generic_info.rb
@@ -1,4 +1,4 @@
 class GenericInfo
 
-	attr_accessor :current_email_address, :is_admin, :is_librarian, :is_user
+	attr_accessor :current_email_address, :is_admin, :is_librarian, :is_user, :user_id, :first_name, :last_name, :phone_number, :error_message
 end
